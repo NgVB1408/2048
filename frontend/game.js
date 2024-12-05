7f5efd222a88091547f81833ecd27b3c0d3ad758
Vinny <176841602+NgVB1408@users.noreply.github.com>
2024-12-05 16:04:27 +0700

    Update game.js

--- frontend/game.js
+++ frontend/game.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.getElementById("grid");
+
+    // Tạo ma trận 4x4
+    const createGrid = () => {
+        for (let i = 0; i < 16; i++) {
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            grid.appendChild(tile);
+        }
+    };
+
+    createGrid();
+});
 
